54b7dc72381a9b3ab4f2cecb904487391ca892a2
mattfnz28 <matiasift2008@gmail.com>
2026-07-21 01:11:54 -0600

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,17 @@
+package funcionesprocesos;
+
+/**
+ *
+ * @author Matias Fernandez
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        numerosPares();
+    }
+    public static void numerosPares(){
+    for (int i =2; i<=100; i+=2){
+        System.out.println(i+" ");
+    }
+        System.out.println();
+}
+}
