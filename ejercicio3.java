a3b5e8fb94e5ddc4c06814068d282f1fe8ff5737
mattfnz28 <matiasift2008@gmail.com>
2026-07-21 01:12:16 -0600

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,15 @@
+package funcionesprocesos;
+
+/**
+ *
+ * @author Matias Fernandez
+ */
+public class ejercicio3 {
+    
+public static void main(String[] args) {
+        saludar("Carlos");
+    }
+    public static void saludar(String nombre) {
+        System.out.println("Bienvenido " + nombre + ".");
+    }
+}
