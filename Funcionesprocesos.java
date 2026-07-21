22869706f8472676a4cf1403df89569f9e2e682b
mattfnz28 <matiasift2008@gmail.com>
2026-07-21 01:12:28 -0600

    Add files via upload

---
+++ Funcionesprocesos.java
@@ -0,0 +1,18 @@
+package funcionesprocesos;
+
+/**
+ *
+ * @author Matias Fernandez
+ */
+public class Funcionesprocesos {
+    public static void main(String[] args) {
+        bienvenida();
+        
+    }
+        public static void bienvenida(){
+        System.out.println("Universidad: Universidad Tecnica de Machala");
+        System.out.println("Materia: Tenicas de Programacion");
+        System.out.println("Estudiante: Jhair ALfredo Ramirez Aguilar");
+    }
+    }
+
